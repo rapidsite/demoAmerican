8a2e903ab7ad2d6e6695c59fb4099484b1c350c1
Rapidsite <128565288+rapidsite@users.noreply.github.com>
2023-04-08 20:43:51 -0500

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,12 @@
 "use strict";
+
+/* Loader */
+$(window).load(function() {
+    $(".loader-item").delay(800).fadeOut();
+    $("#loader, #loader2, #loader3").delay(1300).fadeOut("slow");
+});
+
+
 (function($) {
 
 	$(document).ready(function () {
